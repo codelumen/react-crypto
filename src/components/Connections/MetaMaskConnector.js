ada229dd575ce7d08c5ecc7f689bbcaf377c2b28
anore <66754841+anoredev@users.noreply.github.com>
2022-03-29 02:05:13 +0300

    host

--- src/components/Connections/MetaMaskConnector.js
+++ src/components/Connections/MetaMaskConnector.js
@@ -3,7 +3,7 @@ import { InjectedConnector } from '@web3-react/injected-connector';
 
 export const injectedMetaMask = new InjectedConnector({
     supportedChainIds: [ 1, 56 ],
-    rpc: {
+    rpcUrls: {
         56: "https://rpc.ankr.com/bsc"
     },
 });
